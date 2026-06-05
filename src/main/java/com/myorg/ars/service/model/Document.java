808c307546ad2acf0468d32d7a71533131c69537
crdSDE18 <crdoriety110417@gmail.com>
2026-06-04 19:49:58 -0500

    converted pojo to record

--- src/main/java/com/myorg/ars/service/model/Document.java
+++ src/main/java/com/myorg/ars/service/model/Document.java
@@ -1,14 +1,8 @@
 package com.myorg.ars.service.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
-@AllArgsConstructor
-@Getter
-public class Document {
-    private UUID jobID;
-    private MultipartFile document;
+public record Document(UUID jobID, MultipartFile doc) {
 }
