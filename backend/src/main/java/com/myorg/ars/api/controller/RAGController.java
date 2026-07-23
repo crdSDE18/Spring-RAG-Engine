0a0def57cc9bb13b31f46d3486ebf11a4b879f6c
crdSDE18 <crdoriety110417@gmail.com>
2026-07-23 07:14:23 -0500

    class naem change and try catch around job logic

--- backend/src/main/java/com/myorg/ars/api/controller/RAGController.java
+++ backend/src/main/java/com/myorg/ars/api/controller/RAGController.java
@@ -1,17 +1,16 @@
 package com.myorg.ars.api.controller;
 
 import com.myorg.ars.service.ingestion.DocumentOrchestrator;
-import com.myorg.ars.service.strategy.model.DocumentRequest;
+import com.myorg.ars.service.ingestion.JobService;
+import com.myorg.ars.service.model.DocumentRequest;
+import com.myorg.ars.service.model.job.Job;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
@@ -22,30 +21,42 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class RAGController {
 
-    private final DocumentOrchestrator service;
+    private final DocumentOrchestrator orchestrator;
+    private final JobService service;
 
     @PostMapping(value = "/documents", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<String> uploadDocument(@RequestPart("doc") MultipartFile doc){
+    public ResponseEntity<String> uploadDocument(@RequestPart("doc") MultipartFile doc) {
         log.info("Received Document:{}", doc.getOriginalFilename());
-        Assert.notNull(doc,"No file present");
+        Assert.notNull(doc, "No file present");
         Assert.isTrue(!doc.isEmpty(), "File is empty");
 
-
+        UUID jobId = UUID.randomUUID();
         //TODO design how to not move the actual document in to service domain.
-        DocumentRequest documentRequest = new DocumentRequest(UUID.randomUUID(),doc);
-        log.info("Sending File to orchestrator");
+        DocumentRequest documentRequest = new DocumentRequest(jobId, doc);
 
+        //TODO domain error handling
         try {
-            service.processDocument(documentRequest);
+            service.createJob(jobId);
+        } catch(Exception e){
+            log.error("Failed while sending to decider service", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed while creating job");
         }
-        catch (Exception e){
-            log.error("Failed while sending to decider service",e);
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed ot process document");
+
+        log.info("Sending File to orchestrator");
+        try {
+            orchestrator.processDocument(documentRequest);
+        } catch (Exception e) {
+            log.error("Failed while sending to decider service", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to process document");
 
         }
 
         //TODO implement further endpoints, as this will eventually be async processing
-        return ResponseEntity.status(HttpStatus.CREATED).header("X-JOB-ID", documentRequest.jobID().toString())
+        return ResponseEntity.status(HttpStatus.CREATED).header("X-JOB-ID", jobId.toString())
                 .body("File Successfully saved with job-id in header");
     }
+
+
+
+
 }
