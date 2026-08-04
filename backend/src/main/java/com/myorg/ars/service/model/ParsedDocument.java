00d0357b1667eebf08079a9a76f09917c11f3e12
crdSDE18 <crdoriety110417@gmail.com>
2026-08-03 20:24:08 -0500

    converted metadata to separate object

--- backend/src/main/java/com/myorg/ars/service/model/ParsedDocument.java
+++ backend/src/main/java/com/myorg/ars/service/model/ParsedDocument.java
@@ -2,5 +2,5 @@ package com.myorg.ars.service.model;
 
 import java.util.Map;
 
-public record ParsedDocument(String jobID, String parsedText, Map<String, Object> metadata) {
+public record ParsedDocument(String jobID, String parsedText, DocumentMetadata metadata) {
 }
